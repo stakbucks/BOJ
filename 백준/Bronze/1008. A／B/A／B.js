06a04307d20eb8bf02b6852654422a3a1171e67e
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-02-03 13:52:39 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split(" ").map(Number);
+function solution(i) {
+  console.log(i[0] / i[1]);
+}
+solution(input);
