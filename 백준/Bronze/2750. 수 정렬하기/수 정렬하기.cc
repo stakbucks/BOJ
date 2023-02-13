e795114e5662f078a70e4bdfc597ecf5677ed64c
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-02-13 22:52:26 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	int N;
+	cin >> N;
+	int arr[1001];
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	for (int i = 0; i < N; i++) {
+		cout << arr[i] << '\n';
+	}
+}
