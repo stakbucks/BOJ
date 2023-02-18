06a91f6d42ae7d3d54b7fac354d3ec019186702f
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-02-18 20:00:55 +0900

    [Silver III] Title: 통계학, Time: 552 ms, Memory: 80984 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.js
+++ 백준/Silver/2108. 통계학/통계학.js
@@ -15,16 +15,16 @@ let mode = [];
 let most = 0;
 let count = 0;
 for (let i = 0; i < N; i++) {
-  if (i === N - 1) {
-    count++;
-    if (most === count) {
-      mode.push(arr[i]);
-    }
-    if (count > most) {
-      mode = [arr[i]];
-    }
-  } else {
-    if (arr[i] !== arr[i + 1]) {
+//   if (i === N - 1) {
+//     count++;
+//     if (most === count) {
+//       mode.push(arr[i]);
+//     }
+//     if (count > most) {
+//       mode = [arr[i]];
+//     }
+//   } else {
+    if (arr[i] !== arr[i + 1] || i === N - 1) {
       count++;
       if (most === count) {
         mode.push(arr[i]);
@@ -39,8 +39,8 @@ for (let i = 0; i < N; i++) {
     if (arr[i] === arr[i + 1]) {
       count++;
     }
+  // }
   }
-}
 answer.push(Math.round((sum / N).toFixed(4)));
 answer.push(arr[(N - 1) / 2]);
 if (mode.length >= 2) {
