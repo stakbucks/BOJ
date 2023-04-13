935ea30b48bfb7cbff48f3df503246e6ad8e3fed
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-04-13 19:11:58 +0900

    [Silver III] Title: N과 M (8), Time: 188 ms, Memory: 12748 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+const [N, M] = input[0].split(" ").map(Number);
+
+const arr = [];
+input[1].split(" ").forEach((i) => arr.push(Number(i)));
+arr.sort((a, b) => a - b);
+
+let answer = [];
+let result = [];
+
+function go(idx) {
+  result.push(arr[idx]);
+
+  if (result.length === M) {
+    answer.push(result.join(" "));
+    result.pop();
+    return;
+  }
+
+  for (let i = idx; i < N; i++) {
+    go(i);
+  }
+  result.pop();
+  return;
+}
+
+for (let i = 0; i < N; i++) {
+  result = [];
+  go(i);
+}
+console.log(answer.join("\n"));
