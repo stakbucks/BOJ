9b902722debc8a3e3f4a54881b9695999a251ed6
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-04-19 11:22:16 +0900

    [Silver III] Title: 퇴사, Time: 132 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.js
+++ 백준/Silver/14501. 퇴사/퇴사.js
@@ -9,26 +9,15 @@ const arr = Array.from({ length: N }, (i, v) =>
   input[v + 1].split(" ").map(Number)
 );
 
-function go(idx, memo, nextDay) {
-  maxProfit = Math.max(maxProfit, memo);
-  for (let i = nextDay; i < N; i++) {
-    if (i + arr[i][0] <= N) {
-      go(i, memo + arr[i][1], i + arr[i][0]);
-    }
-  }
-
-  return;
-}
-
+const dp = new Array(N + 1).fill(0);
 function solve() {
   for (let i = 0; i < N; i++) {
-    const memo = arr[i][1];
-    const nextDay = i + arr[i][0];
-    if (nextDay <= N) {
-      go(i, memo, nextDay);
+    if (i + arr[i][0] <= N) {
+      for (let j = i + arr[i][0]; j <= N; j++) {
+        dp[j] = Math.max(dp[j], dp[i] + arr[i][1]);
+      }
     }
   }
-
-  console.log(maxProfit);
+  console.log(Math.max.apply(null, dp));
 }
 solve();
