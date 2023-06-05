12074173606f9856240bb970c4e6cad6a550d1b4
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-06-05 17:00:35 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -0,0 +1,19 @@
+function solution(number, k) {
+    let erasedCnt=0; // 제거한 횟수
+    const stack=[];
+    
+    stack.push(number[0]);
+    for(let i=1; i<number.length; i++){
+        while(stack[stack.length-1] < number[i] && erasedCnt<k ){
+            stack.pop();
+            erasedCnt++;
+        }
+        stack.push(number[i]);
+    }
+    
+    while(erasedCnt < k){
+        stack.pop();
+        erasedCnt++;
+    }
+    return stack.join("");
+}
