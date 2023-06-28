a5d0b8b7d297301ed52ea0faa8a06b3ea7d2b35f
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-06-28 13:39:15 +0900

    [Silver I] Title: 스타트링크, Time: 1048 ms, Memory: 74716 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split(' ');
+
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+  enqueue(value) {
+    this.queue[this.rear++] = value;
+  }
+  dequeue() {
+    const returnValue = this.queue[this.front];
+    delete this.queue[this.front++];
+    return returnValue;
+  }
+  size() {
+    return this.rear - this.front;
+  }
+}
+
+function solution(input) {
+  // 위로 U층, 아래로 D층, 도착 G층, 현재 S층, 총 F층
+  const [F, S, G, U, D] = input.map(Number);
+  const dp = Array(F + 1).fill(Infinity);
+  dp[S] = 0;
+  const queue = new Queue();
+  queue.enqueue(S);
+
+  const set = new Set();
+  set.add(S);
+
+  while (queue.size()) {
+    const s = queue.dequeue();
+    if (s + U <= F && !set.has(s + U)) {
+      queue.enqueue(s + U);
+      set.add(s + U);
+      dp[s + U] = dp[s] + 1;
+    }
+    if (s - D >= 1 && !set.has(s - D)) {
+      queue.enqueue(s - D);
+      set.add(s - D);
+      dp[s - D] = dp[s] + 1;
+    }
+  }
+  dp[G] === Infinity ? console.log('use the stairs') : console.log(dp[G]);
+}
+
+solution(input);
