6e6a82aba3df13861428e9ad73526b375cf1cea7
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-06-28 14:13:37 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 484 ms, Memory: 12164 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+function solution(input) {
+  const test = input.splice(1).map(Number);
+  const answer = [];
+  // 소수 여부 판별 함수
+  const isPrimeNumber = (num) => {
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  };
+  for (const num of test) {
+    let a = num / 2;
+    let b = num / 2;
+    while (1) {
+      if (isPrimeNumber(a) && isPrimeNumber(b)) {
+        answer.push([a, b].join(' '));
+        break;
+      }
+      a--;
+      b++;
+    }
+  }
+  console.log(answer.join('\n'));
+}
+
+solution(input);
