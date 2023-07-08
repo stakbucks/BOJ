ed5b81ebd37a1ac69690853762ac5ca0509dd236
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-07-08 19:53:57 +0900

    [level 2] Title: 모음 사전, Time: 0.78 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -4,23 +4,24 @@ function solution(word) {
   let count = 0;
 
   const stack = [1];
-  while (1) {
-      count++;
-    const current = stack.map((v) => vowels[v]).join('');
-    if (current === word) {
+
+  const convertToString = (arr) => {
+    return arr.map((v) => vowels[v]).join('');
+  };
+
+  while (stack.length) {
+    count++;
+    const string = convertToString(stack);
+    if (string === word) {
       return count;
     }
 
     if (stack.length < 5) {
       stack.push(1);
     } else {
-      while (1) {
+      while (stack.length) {
         const top = stack.pop();
         if (top === 5) {
-          if (!stack.length) {
-            // UUUUU 까지 도달한 경우
-            return count;
-          }
           continue;
         } else {
           stack.push(top + 1);
@@ -29,4 +30,5 @@ function solution(word) {
       }
     }
   }
+  return count;
 }
