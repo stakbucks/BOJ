b7585bcc1bffdbf07916671fce8074d233a3b55b
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-08-26 13:27:52 +0900

    [Gold V] Title: 풍선 맞추기, Time: 1620 ms, Memory: 96164 KB -BaekjoonHub

--- 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.js
+++ 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.js
@@ -3,14 +3,17 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 function solution(input) {
-  const arr = input[1].split(' ').map(Number);
+  const balloons = input[1].split(' ').map(Number);
   const arrows = [];
-  for (const number of arr) {
-    const idx = arrows.indexOf(number);
+  for (const height of balloons) {
+    // 해당 높이의 풍선을 터뜨릴 수 있는 화살이 존재하는지 체크
+    const idx = arrows.indexOf(height);
     if (idx !== -1) {
+      // 존재하면 해당 화살의 높이 -1
       arrows[idx]--;
     } else {
-      arrows.push(number - 1);
+      // 존재하지 않으면 새로운 화살 추가
+      arrows.push(height - 1);
     }
   }
   console.log(arrows.length);
