6c1ee75aed14e58b513573e8e854a5ea074591a2
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-09-04 21:20:15 +0900

    [Gold I] Title: K번째 수, Time: 216 ms, Memory: 11360 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+function solution(input) {
+  const [N, k] = input.map(Number);
+
+  let left = 1;
+  let right = N * N;
+  let result = 0;
+  let mid = Math.floor((left + right) / 2);
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    let count = 0;
+    for (let i = 1; i <= N; i++) {
+      count += getRowCount(i, mid);
+    }
+    if (count >= k) {
+      result = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  function getRowCount(row, mid) {
+    return mid / row >= N ? N : Math.floor(mid / row);
+  }
+
+  console.log(result);
+}
+
+solution(input);
