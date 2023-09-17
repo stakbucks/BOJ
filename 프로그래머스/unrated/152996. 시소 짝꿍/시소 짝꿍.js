3c47ed33fd7e2fca880c320db9112b89c5dbd767
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-09-17 23:26:29 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.js
@@ -0,0 +1,17 @@
+function solution(weights) {
+  const distances = [1, 2 / 3, 2 / 4, 3 / 4];
+  const map = new Map();
+  let count = 0;
+  weights = weights.sort((a, b) => a - b);
+  for (const weight of weights) {
+    for (const dist of distances) {
+      if (map.has(weight * dist)) {
+        count += map.get(weight * dist);
+      }
+    }
+    map.set(weight, (map.get(weight) || 0) + 1);
+  }
+  return count;
+}
+
+
