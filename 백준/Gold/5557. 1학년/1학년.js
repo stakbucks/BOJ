845305ac7dc1884dd416657db2823287fe4617a7
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-10-19 13:30:54 +0900

    [Gold V] Title: 1학년, Time: 116 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Gold/5557. 1학년/1학년.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+function solution(input) {
+  const numbers = input[1].split(' ').map(Number);
+  const target = numbers.pop();
+
+  const dp = Array.from({length: numbers.length}, () => Array(21).fill(BigInt(0)));
+  dp[0][numbers[0]] = BigInt(1);
+
+  for (let i = 1; i < numbers.length; i++) {
+    const number = numbers[i];
+    for (let j = 0; j <= 20; j++) {
+      if (j - number >= 0) dp[i][j] += dp[i - 1][j - number];
+      if (j + number <= 20) dp[i][j] += dp[i - 1][j + number];
+    }
+  }
+  console.log(dp[numbers.length - 1][target].toString());
+}
+
+solution(input);
