335c4e5011ff36756cfa0d47e0efd94013d77306
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-10-23 13:11:03 +0900

    [Gold IV] Title: 괄호, Time: 1244 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Gold/10422. 괄호/괄호.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
+
+const DIVIDE_NUM = BigInt(1000000007);
+
+function solution(input) {
+  const [T, ...L] = input;
+  const answer = [];
+  const dp = Array(2501).fill(BigInt(0));
+  dp[0] = BigInt(1);
+  dp[1] = BigInt(1);
+
+  for (let i = 2; i < 2501; i++) {
+    for (let j = 0; j <= i - 1; j++) {
+      dp[i] = (dp[i] + dp[j] * dp[i - 1 - j]) % DIVIDE_NUM;
+    }
+  }
+  for (const length of L) {
+    if (length % 2 !== 0) answer.push(0);
+    else answer.push(dp[length / 2].toString());
+  }
+
+  console.log(answer.join('\n'));
+}
+solution(input);
