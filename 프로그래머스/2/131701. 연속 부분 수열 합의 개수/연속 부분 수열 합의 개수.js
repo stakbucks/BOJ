cdc6dc278f3d4c2f544c5ea1230a6f2d31dc5e07
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-11-20 16:39:34 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 95.14 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,20 @@
+function solution(elements) {
+  // 결과 값 중복없이 저장
+  const set = new Set();
+
+  const dp = Array.from({length: elements.length}, () => Array(elements.length).fill(0));
+  dp[0] = [...elements];
+  elements.forEach((element) => {
+    set.add(element);
+  });
+
+  for (let i = 1; i < elements.length; i++) {
+    // 원형 형태로 만들기
+    const circular = [...elements].concat(elements.slice(0, i));
+    for (let j = 0; j < elements.length; j++) {
+      dp[i][j] = dp[i - 1][j] + circular[j + i];
+      set.add(dp[i][j])
+    }
+  }
+  return set.size;
+}
