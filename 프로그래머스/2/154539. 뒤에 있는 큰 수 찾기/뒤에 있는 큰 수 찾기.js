25301d20e7043894f8c99ac898694fd7aeadbf39
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-11-21 17:08:51 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 62.91 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,37 +1,28 @@
-class Queue{
-    queue=[];
-    front=0;
-    rear=0;
-    
-    enqueue(value){
-        this.queue[this.rear]=value;
-        this.rear++;
-    }
-    dequeue(){
-        const returnValue=this.queue[this.front];
-        delete this.queue[this.front++];
-        return returnValue;
-    }
-    size(){
-        return this.rear-this.front;
-    }
-}
-
 function solution(numbers) {
-    const stack=[];
-    const answer=Array(numbers.length).fill(-1);
-   
-    for(let i=0; i<numbers.length; i++){
-        const number=numbers[i];
-        while(stack.length && stack.at(-1)[0]<number){
-            answer[stack.pop()[1]]=number;
-        }
-        stack.push([number, i]);
-    }
-    
-    return answer;
-}
+  let answer = [];
+  let max = 0;
+  let search_arr = [];
 
+  for (let i = numbers.length - 1; i >= 0; i--) {
+    if (numbers[i] >= max) {
+      max = numbers[i];
+      answer.push(-1);
+      search_arr = [numbers[i]];
+    } else {
+      while (1) {
+        if (numbers[i] < search_arr.at(-1)) {
+          answer.push(search_arr.at(-1));
+          search_arr.push(numbers[i]);
+          break;
+        } else {
+          search_arr.pop();
+        }
+      }
+    }
+  }
+
+  return answer.reverse();
+}
 // [ 9, , 5, 3,  ]
 
 // [5,]
