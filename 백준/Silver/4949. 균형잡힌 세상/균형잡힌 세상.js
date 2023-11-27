1414b7249b44903125b69053fca11cf2f4d6def7
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-11-27 14:47:30 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 208 ms, Memory: 12224 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.js
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.js
@@ -9,34 +9,34 @@ function solution(input) {
 
     const stack = [];
 
-    let flag = true;
+    let result = 'yes';
 
     [...line].forEach((letter) => {
+      // 여는 괄호는 스택에 넣어준다
       if (letter === '[' || letter === '(') {
         stack.push(letter);
       }
+
+      // 닫는 괄호는 스택의 top과 비교하여 쌍을 이루는지 체크
       if (letter === ')') {
         if (stack.at(-1) !== '(') {
-          flag = false;
+          result = 'no';
           return;
         }
         stack.pop();
       }
       if (letter === ']') {
         if (stack.at(-1) !== '[') {
-          flag = false;
+          result = 'no';
           return;
         }
         stack.pop();
       }
     });
-    if (stack.length) flag = false;
 
-    if (flag) {
-      answer.push('yes');
-    } else {
-      answer.push('no');
-    }
+    if (stack.length) result = 'no'; // 괄호가 안닫힌 채로 문자열이 끝나는 경우!
+
+    answer.push(result);
   });
 
   console.log(answer.join('\n'));
