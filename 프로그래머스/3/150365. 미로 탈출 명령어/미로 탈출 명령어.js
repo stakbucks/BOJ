2213d9fc6a8a5f4bfe6a4fb06d761b3c89824c58
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-12-25 16:07:37 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -1,58 +1,51 @@
-const moves=[
-    {
-        direction:'d',
-        move:[1,0]
-    },
-    {
-        direction:'l',
-        move:[0,-1]
-    },
-    {
-        direction:'r',
-        move:[0,1]
-    },
-    {
-        direction:'u',
-        move:[-1,0]
-    }
-]
+const MOVES = [
+  {
+    direction: 'd',
+    move: [1, 0],
+  },
+  {
+    direction: 'l',
+    move: [0, -1],
+  },
+  {
+    direction: 'r',
+    move: [0, 1],
+  },
+  {
+    direction: 'u',
+    move: [-1, 0],
+  },
+];
 
 function solution(n, m, x, y, r, c, k) {
- 
-    let [currentX, currentY]=[x,y];
-    
-    let answer='';
+  let [currentX, currentY] = [x, y];
 
-    
-    const minDist=Math.abs(currentX-r)+Math.abs(currentY-c)
-    const res=k-minDist
-    if (minDist % 2 !== k % 2 || res < 0) return "impossible";
-    
-    while(1){
-        if(Math.abs(currentX-r)+Math.abs(currentY-c)>=k) break;
+  let answer = '';
 
-        
-        for(const {direction, move} of moves){
-            const [dx,dy]=move;
-            const [nx, ny]=[dx+currentX, dy+currentY];
-            if(nx>0 && nx<=n && ny>0 && ny<=m){
-                answer+=direction;
-                [currentX, currentY]=[nx, ny];
-                break;
-            }
-        }
-        k--;
+  const minDist = Math.abs(currentX - r) + Math.abs(currentY - c); // 탈출 위치까지 움직여야 하는 최소 거리
+
+  if ((k - minDist) % 2 || k < minDist) return 'impossible';
+
+  while (1) {
+    const minDist = Math.abs(currentX - r) + Math.abs(currentY - c);
+    if (minDist === k) break; // minDist===k 이면 정답은 하나다
+
+    for (const {direction, move} of MOVES) {
+      const [dx, dy] = move;
+      const [nx, ny] = [dx + currentX, dy + currentY];
+      if (nx > 0 && nx <= n && ny > 0 && ny <= m) {
+        answer += direction;
+        [currentX, currentY] = [nx, ny];
+        break;
+      }
     }
-  if (currentX < r) answer += "d".repeat(r - currentX);
-  if (currentY > c) answer += "l".repeat(currentY - c);
-  if (currentY < c) answer += "r".repeat(c - currentY);
-  if (currentX > r) answer += "u".repeat(currentX - r);
-    
-    
-    
-    return answer;
+    k--;
+  }
+
+  if (currentX < r) answer += 'd'.repeat(r - currentX);
+  if (currentY > c) answer += 'l'.repeat(currentY - c);
+  if (currentY < c) answer += 'r'.repeat(c - currentY);
+  if (currentX > r) answer += 'u'.repeat(currentX - r);
+
+  return answer;
 }
-
-// dll
-
-// [d,u,l,r]
